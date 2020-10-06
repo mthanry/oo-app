93feffae07f0fca1c8145f1eb928fa50d8b3f323
mthanry <mthanry@gmail.com>
2020-10-06 18:25:49 +0000

    Week 2 work

---
+++ wk2.py
@@ -0,0 +1,15 @@
+
+
+name = True         # boolean
+name = 1            # integer
+name = 1.0          # float
+name = "1,2,3,4"    # string
+name = [1,2,3,4]    # list
+name = (1,2,3,4)    # tuple, cannot be modified
+name = {"first":"John", "last":"Doe"}   #dictionary
+
+list = [1,2,3]
+print(list[2])
+
+list.append(4)
+print(list)
